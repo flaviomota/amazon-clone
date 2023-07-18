6e0056c6acfe608df4c57f835d445efc726b122e
flaviomota <flaviofilipemota@gmail.com>
2023-07-18 10:30:19 +0100

    Login, Registration and Forget Password

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -1,7 +1,21 @@
 <template>
-    <button
-        class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
-    >
-        <slot />
-    </button>
+	<button
+		:type="type"
+		class="
+			w-full
+			text-center
+			items-center
+			px-4
+			py-1.5
+			bg-[#fcba1f]
+			border
+			border-gray-600
+			rounded-sm
+			text-sm
+			font-extrabold
+			text-black
+		"
+	>
+		<slot />
+	</button>
 </template>
